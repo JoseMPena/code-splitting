493a45641d98ef9123a7a927b0aac06172e31204
Jose M Peña <josmpena91@gmail.com>
2020-02-12 17:20:36 +0100

    save state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Page1 from './Page1'
+// import Page2 from './Page2'
+// import Page3 from './Page3'
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'page1',
+      component: ''
+    }
+  }
+
+  onRouteChange = (route) => {
+      // without codesplitting:
+    // this.setState({route: route})
+    // with codesplitting
+    if (this.state.route === 'page1'){
+      this.setState({route})
+    } else if (this.state.route === 'page2') {
+      import('./Page2').then((Page2) => {
+        console.log(Page2);
+        this.setState({route: route, component: Page2.default})
+      })
+    } else if (this.state.route === 'page3') {
+      import('./Page3').then((Page3) => {
+        this.setState({route: route, component: Page3.default})
+      })
+    }
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    // if (this.state.route === 'page1'){
+    //     //   return <Page1 onRouteChange={this.onRouteChange}/>
+    //     // } else if (this.state.route === 'page2') {
+    //     //  return <Page2 onRouteChange={this.onRouteChange}/>
+    //     // } else if (this.state.route === 'page3') {
+    //     //   return <Page3 onRouteChange={this.onRouteChange}/>
+    //     // }
+
+    if (this.state.route === 'page1'){
+      return <Page1 onRouteChange={this.onRouteChange}/>
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange}/>
+    }
   }
 }
 
